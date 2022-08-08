20411040ecac8f373554eba05abd19a7c71a86fa
Chantal Hari <chantal.hari@bluewin.ch>
2022-08-08 12:21:28 +0000

    Add new file

---
+++ get_IUCN_habitat_classification.R
@@ -0,0 +1,11 @@
+library("rredlist")
+#https://cran.r-project.org/web/packages/rredlist/rredlist.pdf
+
+
+IUCN_REDLIST_KEY='3e50039bd95a0de7b3e8c4a470d9dfb78c15c104aae186169131e7ed356aa42a'
+
+#save text file with habitat classification ifnormation per species 
+habitats <- rl_habitats(name='Elephas maximus',key=IUCN_REDLIST_KEY, region="global",parse=T)
+
+#pull together with: 
+find $OUTPUT. -name "*.txt" | xargs -n 1 tail -n +2 >> /storage/harichan/chelsa_V2/WRF/w5e5_wrf_all_",i,".txt 
