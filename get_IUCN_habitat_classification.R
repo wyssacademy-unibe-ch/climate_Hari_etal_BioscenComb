8a83f059a34cd841936dda118044572645194299
Chantal Hari <chantal.hari@bluewin.ch>
2022-08-23 13:50:33 +0000

    Update get_IUCN_habitat_classification.R

--- get_IUCN_habitat_classification.R
+++ get_IUCN_habitat_classification.R
@@ -1,7 +1,7 @@
 library("rredlist")
 #https://apiv3.iucnredlist.org/api/v3/docs#habitat-name
 #https://cran.r-project.org/web/packages/rredlist/rredlist.pdf
-
+#
 #My IUCN-API token
 IUCN_REDLIST_KEY='3e50039bd95a0de7b3e8c4a470d9dfb78c15c104aae186169131e7ed356aa42a'
 
