526e45f9994fdc5a102d9b5aefbd4c1c14b9ac83
Chantal Hari <chantal.hari@bluewin.ch>
2022-10-07 14:18:56 +0000

    Update validation_bioclimtatic_data.R

--- validation_bioclimtatic_data.R
+++ validation_bioclimtatic_data.R
@@ -554,22 +554,97 @@ bioclim_ssp126_2080_landonly <- bind_rows(`bioclim_gfdl-esm4_ssp126_2080_landonl
                                          `bioclim_mri-esm2-0_ssp126_2080_landonly`,
                                          `bioclim_ukesm1-0-ll_ssp126_2080_landonly`)
 
-bio1_rcp26_ssp126_2080 <- all <- merge(bioclim_rcp26_2080_landonly[,1:3], bioclim_ssp126_2080_landonly[,1:3], by=c("x","y"))
-bio2_rcp26_ssp126_2080 <- all <- merge(bioclim_rcp26_2080_landonly[,1:2,4], bioclim_ssp126_2080_landonly[,1:2,4], by=c("x","y"))
-bio3_rcp26_ssp126_2080 <- all <- merge(bioclim_rcp26_2080_landonly[,1:2,5], bioclim_ssp126_2080_landonly[,1:2,5], by=c("x","y"))
-bio4_rcp26_ssp126_2080 <- all <- merge(bioclim_rcp26_2080_landonly[,1:2,6], bioclim_ssp126_2080_landonly[,1:2,6], by=c("x","y"))
-bio5_rcp26_ssp126_2080 <- all <- merge(bioclim_rcp26_2080_landonly[,1:2,7], bioclim_ssp126_2080_landonly[,1:2,7], by=c("x","y"))
-bio6_rcp26_ssp126_2080 <- all <- merge(bioclim_rcp26_2080_landonly[,1:2,8], bioclim_ssp126_2080_landonly[,1:2,8], by=c("x","y"))
-bio7_rcp26_ssp126_2080 <- all <- merge(bioclim_rcp26_2080_landonly[,1:2,9], bioclim_ssp126_2080_landonly[,1:2,9], by=c("x","y"))
+all_bios_rcp26_ssp126_2080 <- merge(bioclim_rcp26_2080_landonly, bioclim_ssp126_2080_landonly, by=c("x","y"))
 
-bios <- c(1:19)
-cols <- c(2:21)
-for (i in bios){
-  for (j in cols){
-    assign(paste0('bio',i,'_rcp26_ssp126_2080')),merge(paste0('bioclim_rcp26_2080_landonly[,1:2,',j,']), paste0(bioclim_ssp126_2080_landonly[,1:2',j,'], by=c("x","y"))'))
-  }
-}
+bio1 <- data.frame(mean_bio1_2b=character(0), mean_bio1_3b=character(0), diff_mean=character(),sd_bio1_2b=character(0), sd_bio1_3b=character(0),diff_sd=character(),cor_bio1=character(0))
 
-assign(paste0('chelsa_w5e5_',s,'_',i,'_',year,'_df'), as.data.frame(paste0('chelsa_w5e5_',s,'_',i,'_',year)))
+bio1 <- rbind(bio1, data.frame(mean_bio1_2b=mean(all_bios_rcp26_ssp126_2080$bio1.x), mean_bio1_3b=mean(all_bios_rcp26_ssp126_2080$bio1.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio1.x)- mean(all_bios_rcp26_ssp126_2080$bio1.y), sd_bio1_2b=sd(all_bios_rcp26_ssp126_2080$bio1.x), sd_bio1_3b=sd(all_bios_rcp26_ssp126_2080$bio1.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio1.x)-sd(all_bios_rcp26_ssp126_2080$bio1.y), cor_bio1=cor(all_bios_rcp26_ssp126_2080$bio1.x,all_bios_rcp26_ssp126_2080$bio1.y)))
+
+bio2 <- data.frame(mean_bio2_2b=character(0), mean_bio2_3b=character(0), diff_mean=character(),sd_bio2_2b=character(0), sd_bio2_3b=character(0),diff_sd=character(),cor_bio2=character(0))
+
+bio2 <- rbind(bio2, data.frame(mean_bio2_2b=mean(all_bios_rcp26_ssp126_2080$bio2.x), mean_bio2_3b=mean(all_bios_rcp26_ssp126_2080$bio2.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio2.x)- mean(all_bios_rcp26_ssp126_2080$bio2.y), sd_bio2_2b=sd(all_bios_rcp26_ssp126_2080$bio2.x), sd_bio2_3b=sd(all_bios_rcp26_ssp126_2080$bio2.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio2.x)-sd(all_bios_rcp26_ssp126_2080$bio2.y), cor_bio2=cor(all_bios_rcp26_ssp126_2080$bio2.x,all_bios_rcp26_ssp126_2080$bio2.y)))
 
 
+bio3 <- data.frame(mean_bio3_2b=character(0), mean_bio3_3b=character(0), diff_mean=character(),sd_bio3_2b=character(0), sd_bio3_3b=character(0),diff_sd=character(),cor_bio3=character(0))
+
+bio3 <- rbind(bio3, data.frame(mean_bio3_2b=mean(all_bios_rcp26_ssp126_2080$bio3.x), mean_bio3_3b=mean(all_bios_rcp26_ssp126_2080$bio3.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio3.x)- mean(all_bios_rcp26_ssp126_2080$bio3.y), sd_bio3_2b=sd(all_bios_rcp26_ssp126_2080$bio3.x), sd_bio3_3b=sd(all_bios_rcp26_ssp126_2080$bio3.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio3.x)-sd(all_bios_rcp26_ssp126_2080$bio3.y), cor_bio3=cor(all_bios_rcp26_ssp126_2080$bio3.x,all_bios_rcp26_ssp126_2080$bio3.y)))
+
+
+bio4 <- data.frame(mean_bio4_2b=character(0), mean_bio4_3b=character(0), diff_mean=character(),sd_bio4_2b=character(0), sd_bio4_3b=character(0),diff_sd=character(),cor_bio4=character(0))
+
+bio4 <- rbind(bio4, data.frame(mean_bio4_2b=mean(all_bios_rcp26_ssp126_2080$bio4.x), mean_bio4_3b=mean(all_bios_rcp26_ssp126_2080$bio4.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio4.x)- mean(all_bios_rcp26_ssp126_2080$bio4.y), sd_bio4_2b=sd(all_bios_rcp26_ssp126_2080$bio4.x), sd_bio4_3b=sd(all_bios_rcp26_ssp126_2080$bio4.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio4.x)-sd(all_bios_rcp26_ssp126_2080$bio4.y), cor_bio4=cor(all_bios_rcp26_ssp126_2080$bio4.x,all_bios_rcp26_ssp126_2080$bio4.y)))
+
+
+bio5 <- data.frame(mean_bio5_2b=character(0), mean_bio5_3b=character(0), diff_mean=character(),sd_bio5_2b=character(0), sd_bio5_3b=character(0),diff_sd=character(),cor_bio5=character(0))
+
+bio5 <- rbind(bio5, data.frame(mean_bio5_2b=mean(all_bios_rcp26_ssp126_2080$bio5.x), mean_bio5_3b=mean(all_bios_rcp26_ssp126_2080$bio5.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio5.x)- mean(all_bios_rcp26_ssp126_2080$bio5.y), sd_bio5_2b=sd(all_bios_rcp26_ssp126_2080$bio5.x), sd_bio5_3b=sd(all_bios_rcp26_ssp126_2080$bio5.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio5.x)-sd(all_bios_rcp26_ssp126_2080$bio5.y), cor_bio5=cor(all_bios_rcp26_ssp126_2080$bio5.x,all_bios_rcp26_ssp126_2080$bio5.y)))
+
+
+bio6 <- data.frame(mean_bio6_2b=character(0), mean_bio6_3b=character(0), diff_mean=character(),sd_bio6_2b=character(0), sd_bio6_3b=character(0),diff_sd=character(),cor_bio6=character(0))
+
+bio6 <- rbind(bio6, data.frame(mean_bio6_2b=mean(all_bios_rcp26_ssp126_2080$bio6.x), mean_bio6_3b=mean(all_bios_rcp26_ssp126_2080$bio6.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio6.x)- mean(all_bios_rcp26_ssp126_2080$bio6.y), sd_bio6_2b=sd(all_bios_rcp26_ssp126_2080$bio6.x), sd_bio6_3b=sd(all_bios_rcp26_ssp126_2080$bio6.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio6.x)-sd(all_bios_rcp26_ssp126_2080$bio6.y), cor_bio6=cor(all_bios_rcp26_ssp126_2080$bio6.x,all_bios_rcp26_ssp126_2080$bio6.y)))
+
+
+bio7 <- data.frame(mean_bio7_2b=character(0), mean_bio7_3b=character(0), diff_mean=character(),sd_bio7_2b=character(0), sd_bio7_3b=character(0),diff_sd=character(),cor_bio7=character(0))
+
+bio7 <- rbind(bio7, data.frame(mean_bio7_2b=mean(all_bios_rcp26_ssp126_2080$bio7.x), mean_bio7_3b=mean(all_bios_rcp26_ssp126_2080$bio7.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio7.x)- mean(all_bios_rcp26_ssp126_2080$bio7.y), sd_bio7_2b=sd(all_bios_rcp26_ssp126_2080$bio7.x), sd_bio7_3b=sd(all_bios_rcp26_ssp126_2080$bio7.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio7.x)-sd(all_bios_rcp26_ssp126_2080$bio7.y), cor_bio7=cor(all_bios_rcp26_ssp126_2080$bio7.x,all_bios_rcp26_ssp126_2080$bio7.y)))
+
+
+bio8 <- data.frame(mean_bio8_2b=character(0), mean_bio8_3b=character(0), diff_mean=character(),sd_bio8_2b=character(0), sd_bio8_3b=character(0),diff_sd=character(),cor_bio8=character(0))
+
+bio8 <- rbind(bio8, data.frame(mean_bio8_2b=mean(all_bios_rcp26_ssp126_2080$bio8.x), mean_bio8_3b=mean(all_bios_rcp26_ssp126_2080$bio8.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio8.x)- mean(all_bios_rcp26_ssp126_2080$bio8.y), sd_bio8_2b=sd(all_bios_rcp26_ssp126_2080$bio8.x), sd_bio8_3b=sd(all_bios_rcp26_ssp126_2080$bio8.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio8.x)-sd(all_bios_rcp26_ssp126_2080$bio8.y), cor_bio8=cor(all_bios_rcp26_ssp126_2080$bio8.x,all_bios_rcp26_ssp126_2080$bio8.y)))
+
+
+bio9 <- data.frame(mean_bio9_2b=character(0), mean_bio9_3b=character(0), diff_mean=character(),sd_bio9_2b=character(0), sd_bio9_3b=character(0),diff_sd=character(),cor_bio9=character(0))
+
+bio9 <- rbind(bio9, data.frame(mean_bio9_2b=mean(all_bios_rcp26_ssp126_2080$bio9.x), mean_bio9_3b=mean(all_bios_rcp26_ssp126_2080$bio9.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio9.x)- mean(all_bios_rcp26_ssp126_2080$bio9.y), sd_bio9_2b=sd(all_bios_rcp26_ssp126_2080$bio9.x), sd_bio9_3b=sd(all_bios_rcp26_ssp126_2080$bio9.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio9.x)-sd(all_bios_rcp26_ssp126_2080$bio9.y), cor_bio9=cor(all_bios_rcp26_ssp126_2080$bio9.x,all_bios_rcp26_ssp126_2080$bio9.y)))
+
+
+bio10 <- data.frame(mean_bio10_2b=character(0), mean_bio10_3b=character(0), diff_mean=character(),sd_bio10_2b=character(0), sd_bio10_3b=character(0),diff_sd=character(),cor_bio10=character(0))
+
+bio10 <- rbind(bio10, data.frame(mean_bio10_2b=mean(all_bios_rcp26_ssp126_2080$bio10.x), mean_bio10_3b=mean(all_bios_rcp26_ssp126_2080$bio10.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio10.x)- mean(all_bios_rcp26_ssp126_2080$bio10.y), sd_bio10_2b=sd(all_bios_rcp26_ssp126_2080$bio10.x), sd_bio10_3b=sd(all_bios_rcp26_ssp126_2080$bio10.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio10.x)-sd(all_bios_rcp26_ssp126_2080$bio10.y), cor_bio10=cor(all_bios_rcp26_ssp126_2080$bio10.x,all_bios_rcp26_ssp126_2080$bio10.y)))
+
+
+
+bio11 <- data.frame(mean_bio11_2b=character(0), mean_bio11_3b=character(0), diff_mean=character(),sd_bio11_2b=character(0), sd_bio11_3b=character(0),diff_sd=character(),cor_bio11=character(0))
+
+bio11 <- rbind(bio11, data.frame(mean_bio11_2b=mean(all_bios_rcp26_ssp126_2080$bio11.x), mean_bio11_3b=mean(all_bios_rcp26_ssp126_2080$bio11.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio11.x)- mean(all_bios_rcp26_ssp126_2080$bio11.y), sd_bio11_2b=sd(all_bios_rcp26_ssp126_2080$bio11.x), sd_bio11_3b=sd(all_bios_rcp26_ssp126_2080$bio11.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio11.x)-sd(all_bios_rcp26_ssp126_2080$bio11.y), cor_bio11=cor(all_bios_rcp26_ssp126_2080$bio11.x,all_bios_rcp26_ssp126_2080$bio11.y)))
+
+
+bio12 <- data.frame(mean_bio12_2b=character(0), mean_bio12_3b=character(0), diff_mean=character(),sd_bio12_2b=character(0), sd_bio12_3b=character(0),diff_sd=character(),cor_bio12=character(0))
+
+bio12 <- rbind(bio12, data.frame(mean_bio12_2b=mean(all_bios_rcp26_ssp126_2080$bio12.x), mean_bio12_3b=mean(all_bios_rcp26_ssp126_2080$bio12.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio12.x)- mean(all_bios_rcp26_ssp126_2080$bio12.y), sd_bio12_2b=sd(all_bios_rcp26_ssp126_2080$bio12.x), sd_bio12_3b=sd(all_bios_rcp26_ssp126_2080$bio12.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio12.x)-sd(all_bios_rcp26_ssp126_2080$bio12.y), cor_bio12=cor(all_bios_rcp26_ssp126_2080$bio12.x,all_bios_rcp26_ssp126_2080$bio12.y)))
+
+
+bio13 <- data.frame(mean_bio13_2b=character(0), mean_bio13_3b=character(0), diff_mean=character(),sd_bio13_2b=character(0), sd_bio13_3b=character(0),diff_sd=character(),cor_bio13=character(0))
+
+bio13 <- rbind(bio13, data.frame(mean_bio13_2b=mean(all_bios_rcp26_ssp126_2080$bio13.x), mean_bio13_3b=mean(all_bios_rcp26_ssp126_2080$bio13.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio13.x)- mean(all_bios_rcp26_ssp126_2080$bio13.y), sd_bio13_2b=sd(all_bios_rcp26_ssp126_2080$bio13.x), sd_bio13_3b=sd(all_bios_rcp26_ssp126_2080$bio13.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio13.x)-sd(all_bios_rcp26_ssp126_2080$bio13.y), cor_bio13=cor(all_bios_rcp26_ssp126_2080$bio13.x,all_bios_rcp26_ssp126_2080$bio13.y)))
+
+
+bio14 <- data.frame(mean_bio14_2b=character(0), mean_bio14_3b=character(0), diff_mean=character(),sd_bio14_2b=character(0), sd_bio14_3b=character(0),diff_sd=character(),cor_bio14=character(0))
+
+bio14 <- rbind(bio14, data.frame(mean_bio14_2b=mean(all_bios_rcp26_ssp126_2080$bio14.x), mean_bio14_3b=mean(all_bios_rcp26_ssp126_2080$bio14.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio14.x)- mean(all_bios_rcp26_ssp126_2080$bio14.y), sd_bio14_2b=sd(all_bios_rcp26_ssp126_2080$bio14.x), sd_bio14_3b=sd(all_bios_rcp26_ssp126_2080$bio14.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio14.x)-sd(all_bios_rcp26_ssp126_2080$bio14.y), cor_bio14=cor(all_bios_rcp26_ssp126_2080$bio14.x,all_bios_rcp26_ssp126_2080$bio14.y)))
+
+
+bio15 <- data.frame(mean_bio15_2b=character(0), mean_bio15_3b=character(0), diff_mean=character(),sd_bio15_2b=character(0), sd_bio15_3b=character(0),diff_sd=character(),cor_bio15=character(0))
+
+bio15 <- rbind(bio15, data.frame(mean_bio15_2b=mean(all_bios_rcp26_ssp126_2080$bio15.x), mean_bio15_3b=mean(all_bios_rcp26_ssp126_2080$bio15.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio15.x)- mean(all_bios_rcp26_ssp126_2080$bio15.y), sd_bio15_2b=sd(all_bios_rcp26_ssp126_2080$bio15.x), sd_bio15_3b=sd(all_bios_rcp26_ssp126_2080$bio15.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio15.x)-sd(all_bios_rcp26_ssp126_2080$bio15.y), cor_bio15=cor(all_bios_rcp26_ssp126_2080$bio15.x,all_bios_rcp26_ssp126_2080$bio15.y)))
+
+
+bio16 <- data.frame(mean_bio16_2b=character(0), mean_bio16_3b=character(0), diff_mean=character(),sd_bio16_2b=character(0), sd_bio16_3b=character(0),diff_sd=character(),cor_bio16=character(0))
+
+bio16 <- rbind(bio16, data.frame(mean_bio16_2b=mean(all_bios_rcp26_ssp126_2080$bio16.x), mean_bio16_3b=mean(all_bios_rcp26_ssp126_2080$bio16.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio16.x)- mean(all_bios_rcp26_ssp126_2080$bio16.y), sd_bio16_2b=sd(all_bios_rcp26_ssp126_2080$bio16.x), sd_bio16_3b=sd(all_bios_rcp26_ssp126_2080$bio16.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio16.x)-sd(all_bios_rcp26_ssp126_2080$bio16.y), cor_bio16=cor(all_bios_rcp26_ssp126_2080$bio16.x,all_bios_rcp26_ssp126_2080$bio16.y)))
+
+
+bio17 <- data.frame(mean_bio17_2b=character(0), mean_bio17_3b=character(0), diff_mean=character(),sd_bio17_2b=character(0), sd_bio17_3b=character(0),diff_sd=character(),cor_bio17=character(0))
+
+bio17 <- rbind(bio17, data.frame(mean_bio17_2b=mean(all_bios_rcp26_ssp126_2080$bio17.x), mean_bio17_3b=mean(all_bios_rcp26_ssp126_2080$bio17.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio17.x)- mean(all_bios_rcp26_ssp126_2080$bio17.y), sd_bio17_2b=sd(all_bios_rcp26_ssp126_2080$bio17.x), sd_bio17_3b=sd(all_bios_rcp26_ssp126_2080$bio17.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio17.x)-sd(all_bios_rcp26_ssp126_2080$bio17.y), cor_bio17=cor(all_bios_rcp26_ssp126_2080$bio17.x,all_bios_rcp26_ssp126_2080$bio17.y)))
+
+bio18 <- data.frame(mean_bio18_2b=character(0), mean_bio18_3b=character(0), diff_mean=character(),sd_bio18_2b=character(0), sd_bio18_3b=character(0),diff_sd=character(),cor_bio18=character(0))
+
+bio18 <- rbind(bio18, data.frame(mean_bio18_2b=mean(all_bios_rcp26_ssp126_2080$bio18.x), mean_bio18_3b=mean(all_bios_rcp26_ssp126_2080$bio18.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio18.x)- mean(all_bios_rcp26_ssp126_2080$bio18.y), sd_bio18_2b=sd(all_bios_rcp26_ssp126_2080$bio18.x), sd_bio18_3b=sd(all_bios_rcp26_ssp126_2080$bio18.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio18.x)-sd(all_bios_rcp26_ssp126_2080$bio18.y), cor_bio18=cor(all_bios_rcp26_ssp126_2080$bio18.x,all_bios_rcp26_ssp126_2080$bio18.y)))
+
+
+bio19 <- data.frame(mean_bio19_2b=character(0), mean_bio19_3b=character(0), diff_mean=character(),sd_bio19_2b=character(0), sd_bio19_3b=character(0),diff_sd=character(),cor_bio19=character(0))
+
+bio19 <- rbind(bio19, data.frame(mean_bio19_2b=mean(all_bios_rcp26_ssp126_2080$bio19.x), mean_bio19_3b=mean(all_bios_rcp26_ssp126_2080$bio19.y),diff_mean=mean(all_bios_rcp26_ssp126_2080$bio19.x)- mean(all_bios_rcp26_ssp126_2080$bio19.y), sd_bio19_2b=sd(all_bios_rcp26_ssp126_2080$bio19.x), sd_bio19_3b=sd(all_bios_rcp26_ssp126_2080$bio19.y),diff_sd=sd(all_bios_rcp26_ssp126_2080$bio19.x)-sd(all_bios_rcp26_ssp126_2080$bio19.y), cor_bio19=cor(all_bios_rcp26_ssp126_2080$bio19.x,all_bios_rcp26_ssp126_2080$bio19.y)))
