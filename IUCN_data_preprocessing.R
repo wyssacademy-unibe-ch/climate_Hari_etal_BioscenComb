cccda0af5b62c1e4b2a9d9ec9f2873add9817462
Chantal Hari <chantal.hari@bluewin.ch>
2022-10-12 11:49:04 +0000

    Update IUCN_data_preprocessing.R

--- IUCN_data_preprocessing.R
+++ IUCN_data_preprocessing.R
@@ -5,14 +5,22 @@ source("/storage/homefs/ch21o450/scripts/rasterSp/R/rasterizeIUCN.R")
 
 
 filedir="/storage/homefs/ch21o450/data/"
-r_amphibians <- rasterizeIUCN(dsn=paste0(filedir, "MAMMALS.shp"), resolution=0.5, 
+r_mammals <- rasterizeIUCN(dsn=paste0(filedir, "MAMMALS.shp"), resolution=0.5, 
                               seasonal=c(1,2), origin=1, presence=c(1,2), 
                               save=TRUE, path=paste0(filedir, "/SpeciesData/"))
 
-source("/storage/homefs/ch21o450/cripts/rasterSp/R/speciesData.R")
+# Read amphibians
+mammals <-  sf::read_sf(dsn=paste0(filedir, "MAMMALS.shp"))
+mammals %<>% as.data.frame() %>% select(-geometry) %>% 
+  group_by(binomial, presence, origin, seasonal, kingdom, phylum, class, order_, family) %>% 
+  summarise_at("SHAPE_Area", sum)
+
+
+
+source("/storage/homefs/ch21o450/scripts/rasterSp/R/speciesData.R")
 speciesData(species_names=unique(mammals$binomial), 
             path=paste0(filedir, "/SpeciesData/"), 
-            filename="data/mammals_dist.csv.xz")
+            filename="mammals_dist.csv.xz")
 
 ter_mammals_dist <- read.csv("data/ter_mammals_dist.csv.xz")
 
