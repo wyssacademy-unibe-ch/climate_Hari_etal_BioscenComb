1509a2a4553119d6c7fb4bca19871cb247d04746
Chantal Hari <chantal.hari@bluewin.ch>
2022-10-17 07:12:26 +0000

    Update IUCN_data_preprocessing.R

--- IUCN_data_preprocessing.R
+++ IUCN_data_preprocessing.R
@@ -23,11 +23,11 @@ speciesData(species_names=unique(mammals$binomial),
             path=paste0(filedir, "/SpeciesData/"), 
             filename="mammals_dist.csv.xz")
 
-ter_mammals_dist <- read.csv("data/ter_mammals_dist.csv.xz")
+ter_mammals_dist <- read.csv("mammals_dist.csv.xz")
 
 save(ter_mammals_dist, file="data/ter_mammals_dist.rda", compress="xz")
 
-file.remove("data/ter_mammals_dist.csv.xz")
+file.remove("mammals_dist.csv.xz")
 
 # Extract data of non-modelled species
 
@@ -35,11 +35,40 @@ load("data/ter_mammals_dist.rda")
 ter_mammals_dist$group <- "Mammals"
 ter_mammals_dist$presence <- 1
 
-ter_mammals_dist$group <- "Mammals"
-
-
+# Number of records per species
 count_sp_ter_mammal <- ter_mammals_dist %>% 
   group_by(species, group) %>% 
   summarise(sum = sum(presence))
-load("data/odonata_dist.rda")
-load("data/ter_mammals_dist.rda")
+
+# Combine counts per species into one dataframe
+species_presences_alltaxa <- rbind(count_sp_ter_mammal,)
+
+species_presences_smallrange <- species_presences_alltaxa %>% 
+  filter(sum < 10)
+
+ter_mammals_dist_smallrange <- ter_mammals_dist %>% 
+  filter(species %in% species_presences_smallrange$species)
+
+# Save to file
+save(ter_mammals_dist_smallrange, file="data/ter_mammals_dist_smallrange.rda", compress="xz")
+
+## Create dataframe of species range areas and select smallest 15%
+source("/storage/homefs/ch21o450/scripts/rasterSp/R/getRangeArea.R")
+range_mammals <- getRangeArea(dsn=paste0(filedir, "MAMMALS.shp"), 
+                              seasonal=c(1,2), origin=1, presence=c(1,2))
+                              
+ter_mammals_endemic <- range_mammals %>% filter(area <= quantile(range_mammals$area, probs=0.15))
+save(ter_mammals_endemic, file="data/ter_mammals_endemic.rda", compress="xz")
+
+
+# Create dataframe of threatened species (amphibian and mammal files come from ...,
+# bird data is included in the BirdLife checklist files).
+
+#Get information on threat
+load("data/threat_status_mammals.rda")
+ter_mammals_threatened <- ter_mammals_dist %>% 
+  filter(species %in% threat_status_mammals$binomial[threat_status_mammals$code %in% c("EN", "CR", "VU")])
+ter_birds_threatened <- ter_birds_dist %>% 
+  filter(species %in% ter_birds$SCINAME[ter_birds$code %in% c("EN", "CR", "VU")])
+save(amphibians_threatened, file="data/amphibians_threatened.rda", compress="xz")
+save(ter_mammals_threatened, file="data/ter_mammals_threatened.rda", compress="xz")
