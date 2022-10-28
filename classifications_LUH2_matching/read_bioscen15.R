6d781b5ce6e585abfde167530e67fee0c8deace8
Chantal Hari <chantal.hari@bluewin.ch>
2022-10-28 14:31:29 +0000

    Update read_bioscen15.R

--- classifications_LUH2_matching/read_bioscen15.R
+++ classifications_LUH2_matching/read_bioscen15.R
@@ -3,4 +3,10 @@ library(raster) # package for raster manipulation
 library(rgdal) # package for geospatial analysis
 library(ggplot2) # package for plotting
 
-bioscen <- 
+bioscen <- nc_open('/storage/homefs/ch21o450/data/BioScen15/bioscen15-sdm-gam_gfdl-esm2m_ewembi_rcp26_nosoc_co2_mammalsumprob_landonly_30year-mean_2009_2080.nc4')
+# Save the print(nc) dump to a text file
+{
+    sink('/storage/homefs/ch21o450/data/BioScen15/bioscen15-sdm-gam_gfdl-esm2m_ewembi_rcp26_nosoc_co2_mammalsumprob_landonly_30year-mean_2009_2080.nc4')
+ print(nc_data)
+    sink()
+}
