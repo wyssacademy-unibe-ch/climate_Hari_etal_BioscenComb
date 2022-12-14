a855b87085c68ee76eef73357011809940eda793
Chantal Hari <chantal.hari@bluewin.ch>
2022-12-14 15:12:47 +0000

    Update get_IUCN_habitat_classification.R

--- get_IUCN_habitat_classification.R
+++ get_IUCN_habitat_classification.R
@@ -5,13 +5,36 @@ library("rredlist")
 #My IUCN-API token
 IUCN_REDLIST_KEY='3e50039bd95a0de7b3e8c4a470d9dfb78c15c104aae186169131e7ed356aa42a'
 
+
+sourceObs <- "/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/Mammals_GAM_results_climate/"
+resultsPath <- paste0("/storage/homefs/ch21o450/IUCN/Habitat_Classifications/new/")
+
+
+# Get all species pseudoabsence files
+spFiles <- list.files(sourceObs, pattern=".csv.xz", full.names=TRUE)
+
+# Extract species names 
+spNames <- lapply(spFiles,function(sp){
+  name <- lapply(sp, function(x){paste(strsplit(basename(x),"_",fixed=TRUE)[[1]][1:2],collapse=" ")})
+})
+spNames <- unlist(spNames)
+
+# Extract species names 
+basename <- lapply(spFiles,function(sp){
+  name <- lapply(sp, function(x){paste(strsplit(basename(x),"_",fixed=TRUE)[[1]][1:2],collapse="_")})
+})
+basename <- unlist(basename)
+
+
+
+output_list=list()
+
+for(i in 1:length(spNames)){
+ind_hab <- rl_habitats(name=paste0(spNames[i]),key=IUCN_REDLIST_KEY, region="global",parse=T)
+hab <-  as.data.frame(ind_hab)
+write.csv(hab, paste0(resultsPath, basename[i],".csv"))
+}
 #save text file with habitat classification ifnormation per species 
-habitats <- rl_habitats(name='Elephas maximus',key=IUCN_REDLIST_KEY, region="global",parse=T)
 
+habitats <- do.call("rbind", output_list)
 
-#pull together with: 
-find $OUTPUT. -name "*.txt" | xargs -n 1 tail -n +2 >> /storage/harichan/chelsa_V2/WRF/w5e5_wrf_all_",i,".txt 
-
-
-#LUH2 data 
-#https://luh.umd.edu/data.shtml
