79bba6b46de2c5daf0fe5b51c90e4a3b8bfff6a5
Chantal Hari <chantal.hari@bluewin.ch>
2023-03-01 14:48:57 +0000

    Update get_IUCN_habitat_classification.R

--- get_IUCN_habitat_classification.R
+++ get_IUCN_habitat_classification.R
@@ -51,25 +51,54 @@ diffNames <- setdiff(gamNames, gbmNames)
 print(paste0("Reptile species names different between GAM and GBM folders: ", paste(diffNames, collapse=", ")))
 
 
+taxas <- c("Mammals", "Reptiles", "Amphibians")
+
 for (taxa in taxas) {
-  sourceObs <- paste0("/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/", taxa, "_", model, "_results_climate/")
-  resultsPath <- paste0("/storage/homefs/ch21o450/IUCN/Habitat_Classifications/new/", taxa, "/")
-  
-  spFiles <- list.files(sourceObs, pattern=".csv.xz", full.names=TRUE)
-  
-  spNames <- lapply(spFiles, function(sp){
-    name <- lapply(sp, function(x){paste(strsplit(basename(x),"_",fixed=TRUE)[[1]][1:2],collapse=" ")})
-  })
-  spNames <- unlist(spNames)
-  
-  basename <- lapply(spFiles,function(sp){
-    name <- lapply(sp, function(x){paste(strsplit(basename(x),"_",fixed=TRUE)[[1]][1:2],collapse="_")})
-  })
-  basename <- unlist(basename)
+  if (taxa == "Reptiles") {
+    gamObs <- list.files(paste0("/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/", taxa, "_GAM_results_climate/"), pattern=".csv.xz", full.names=TRUE)
+    gbmObs <- list.files(paste0("/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/", taxa, "_GBM_results_climate/"), pattern=".csv.xz", full.names=TRUE)
+    
+    gamNames <- lapply(gamObs, function(obs) {
+      strsplit(basename(obs),"_",fixed=TRUE)[[1]][1:2]
+    })
+    gamNames <- unlist(lapply(gamNames, function(name) paste(name, collapse=" ")))
+    
+    gbmNames <- lapply(gbmObs, function(obs) {
+      strsplit(basename(obs),"_",fixed=TRUE)[[1]][1:2]
+    })
+    gbmNames <- unlist(lapply(gbmNames, function(name) paste(name, collapse=" ")))
+    
+    spNames <- unique(c(gamNames, gbmNames))
+  } else {
+    sourceObs <- paste0("/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/", taxa, "_GAM_results_climate/")
+    resultsPath <- paste0("/storage/homefs/ch21o450/IUCN/Habitat_Classifications/new/", taxa, "/")
+    
+    spFiles <- list.files(sourceObs, pattern=".csv.xz", full.names=TRUE)
+    
+    spNames <- lapply(spFiles, function(sp){
+      name <- lapply(sp, function(x){paste(strsplit(basename(x),"_",fixed=TRUE)[[1]][1:2],collapse=" ")})
+    })
+    spNames <- unlist(spNames)
+    
+    basename <- lapply(spFiles,function(sp){
+      name <- lapply(sp, function(x){paste(strsplit(basename(x),"_",fixed=TRUE)[[1]][1:2],collapse="_")})
+    })
+    basename <- unlist(basename)
+  }
   
   for(i in 1:length(spNames)){
     tryCatch({
       print(i)
+      if (taxa == "Reptile") {
+        if (spNames[i] %in% gamNames) {
+          sourceObs <- "/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/Reptile_GAM_results_climate/"
+        } else {
+          sourceObs <- "/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/Reptile_GBM_results_climate/"
+        }
+        resultsPath <- "/storage/homefs/ch21o450/IUCN/Habitat_Classifications/new/Reptile/"
+        basename[i] <- paste0(strsplit(spNames[i], " ")[[1]][1], "_", strsplit(spNames[i], " ")[[1]][2])
+      }
+      
       ind_hab <- rl_habitats(name=paste0(spNames[i]),key=IUCN_REDLIST_KEY, region="global",parse=T)
       hab <-  as.data.frame(ind_hab)
       write.csv(hab, paste0(resultsPath, basename[i],".csv"))}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
