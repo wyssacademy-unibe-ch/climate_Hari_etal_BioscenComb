300f3bd46f2e65b50459627befc5d4ca285b006e
Chantal Hari <chantal.hari@bluewin.ch>
2023-03-03 07:15:51 +0000

    Update get_IUCN_habitat_classification.R

--- get_IUCN_habitat_classification.R
+++ get_IUCN_habitat_classification.R
@@ -8,7 +8,7 @@ IUCN_REDLIST_KEY='3e50039bd95a0de7b3e8c4a470d9dfb78c15c104aae186169131e7ed356aa4
 models =c("GAM","GBM")
 
 
-taxas <- c("Mammals", "Reptiles", "Amphibians")
+taxas <- c("Reptiles")
 
 for (taxa in taxas) {
   gamObs <- list.files(paste0("/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/", taxa, "_GAM_results_climate/"), pattern=".csv.xz", full.names=TRUE)
@@ -51,7 +51,7 @@ diffNames <- setdiff(gamNames, gbmNames)
 print(paste0("Reptile species names different between GAM and GBM folders: ", paste(diffNames, collapse=", ")))
 
 
-taxas <- c("Mammals", "Reptiles", "Amphibians")
+#taxas <- c("Mammals", "Reptiles", "Amphibians")
 
 for (taxa in taxas) {
   if (taxa == "Reptiles") {
@@ -86,21 +86,22 @@ for (taxa in taxas) {
     basename <- unlist(basename)
   }
   
-  for(i in 1:length(spNames)){
-    tryCatch({
-      print(i)
-      if (taxa == "Reptiles") {
-        if (spNames[i] %in% gamNames) {
-          sourceObs <- "/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/Reptiles_GAM_results_climate/"
-        } else {
-          sourceObs <- "/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/Reptiles_GBM_results_climate/"
-        }
-        resultsPath <- "/storage/homefs/ch21o450/IUCN/Habitat_Classifications/Reptiles/"
-        basename[i] <- paste0(strsplit(spNames[i], " ")[[1]][1], "_", strsplit(spNames[i], " ")[[1]][2])
+for(i in 1:length(spNames)) {
+  tryCatch({
+    print(i)
+    if (taxa == "Reptiles") {
+      if (spNames[i] %in% gamNames) {
+        sourceObs <- "/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/Reptiles_GAM_results_climate/"
+      } else {
+        sourceObs <- "/storage/workspaces/wa_climate/climate_trt/data/BioScen15/individual_projections/Reptiles_GBM_results_climate/"
       }
-      
-      ind_hab <- rl_habitats(name=paste0(spNames[i]),key=IUCN_REDLIST_KEY, region="global",parse=T)
-      hab <-  as.data.frame(ind_hab)
-      write.csv(hab, paste0(resultsPath, basename[i],".csv"))}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
-  }
+      resultsPath <- "/storage/homefs/ch21o450/IUCN/Habitat_Classifications/Reptiles/"
+      spBaseName <- paste0(strsplit(spNames[i], " ")[[1]][1], "_", strsplit(spNames[i], " ")[[1]][2])
+    }
+    
+    ind_hab <- rl_habitats(name=paste0(spNames[i]),key=IUCN_REDLIST_KEY, region="global",parse=T)
+    hab <-  as.data.frame(ind_hab)
+    write.csv(hab, paste0(resultsPath, spBaseName, ".csv"))
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
+
